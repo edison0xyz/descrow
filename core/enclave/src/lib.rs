8fac29753cefd2c6bacf5433740583449d1d2321
Edison Lim <edison.limjh@gmail.com>
2020-04-05 10:50:56 +0800

    build: makefile fixes

--- core/enclave/src/lib.rs
+++ core/enclave/src/lib.rs
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License..
 
-#![crate_name = "sealdatasampleenclave"]
+#![crate_name = "escrowenclave"]
 #![crate_type = "staticlib"]
 
 #![cfg_attr(not(target_env = "sgx"), no_std)]
